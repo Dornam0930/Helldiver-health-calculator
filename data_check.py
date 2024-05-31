afb54642c3dc93530bc0de8cd1b457aebeeb1bd9
Dornam0930 <warhammer925@yahoo.com>
2024-05-30 17:17:52 -0700

    add data check and auto download if files not found

---
+++ data_check.py
@@ -0,0 +1,32 @@
+import os, requests
+
+data_repo = "https://raw.githubusercontent.com/zeddidragon/helldivers-calc/main/data/"
+data_dir = "./Data"
+files_to_retrieve = "files to retrieve.txt"
+
+def data_check():
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+    file_list = open(files_to_retrieve, "r")
+    for file in file_list:
+        stripped_file = file.strip("\n")
+        if stripped_file[0] == "#":
+            continue
+        data_repo_file_path = html_file_path(data_repo, stripped_file)
+        data_dir_file_path = file_path(data_dir, stripped_file)
+        if not check_file(data_dir_file_path):
+            download_file(data_dir_file_path, data_repo_file_path)
+
+def file_path(dir_path, file_name):
+    return os.path.join(dir_path, file_name)
+
+def html_file_path(dir_path, file_name):
+    return f"{dir_path}{file_name}"
+
+def check_file(file_path):
+    return os.path.isfile(file_path)
+
+def download_file(file_path, repo_path):
+    download = requests.get(repo_path)
+    new_file = open(file_path, "w")
+    new_file.write(download.text)
---
+++ main.py
@@ -0,0 +1,7 @@
+import os, requests
+from data_check import data_check
+
+def main():
+    data_check()
+
+main()
